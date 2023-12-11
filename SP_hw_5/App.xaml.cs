7254cdac6afb9a29d720c8d137dad5b8f71a5904
sks <ssurovneva@mail.ru>
2023-12-11 23:24:49 +0300

    Add project files.

---
+++ SP_hw_5/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SP_hw_5
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ SP_hw_5/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SP_hw_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Thread thread = new Thread(ReadFile);
+            thread.Start();
+          
+        }
+
+        public string path = "result.txt"; 
+        
+        public ConcurrentBag<string> pointList = new ConcurrentBag<string>(); 
+        Point position = new Point();
+
+        //List<string> listik = new List<string>();
+
+       
+
+        private void Window__MouseMove(object sender, MouseEventArgs e)
+        {          
+            position = Mouse.GetPosition(this);
+            pointList.Add($"X: {(int)position.X} Y: {(int)position.Y}  {DateTime.Now.ToString("HH:mm:ss:ff")}\n");
+            tb.Text = "X: " + (int)position.X + " " + "Y: " + (int)position.Y + " " + DateTime.Now.ToString("HH:mm:ss:ff");
+            
+            //listik.Add($"X: {(int)position.X} Y: {(int)position.Y}  {DateTime.Now.ToString("HH:mm:ss:ff")}\n");            
+        }
+
+        private void ReadFile()
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);                
+            }
+
+            File.WriteAllLines(path, pointList/*pointList.ToString()*/);
+
+            //foreach (var item in listik)
+            //{
+            //    File.WriteAllLines(path, listik);
+            //}
+
+           /* using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(listik.Count);
+                
+            }*/
+        }
+
+        
+    }
+}
